d499d43f3f972af68819f541712ce023d81aafcf
Chester-232 <jairadizon558@gmail.com>
2025-05-04 00:56:20 +0800

    created the (tabs) and added the assets and thier declarations

--- app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary font-bold">4769 + 2729 {4769 + 2729}</Text>
-    </View>
-  );
-}
